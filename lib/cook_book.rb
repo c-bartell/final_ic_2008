65593bf0a0a6d29d5ee33eaeec9902495aa01a28
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 10:01:28 -0600

    Define class CookBook

---
+++ lib/cook_book.rb
@@ -0,0 +1,5 @@
+require './lib/recipe'
+
+class CookBook
+
+end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/cookbook'
+require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
   def test_it_exists
