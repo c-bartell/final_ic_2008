93754e2e57991f86f0f019a82cd7fd0873757e6d
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 09:44:28 -0600

    Add test_it_exists

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -3,5 +3,9 @@ require 'minitest/pride'
 require './lib/recipe'
 
 class RecipeTest < Minitest::Test
-  
+  def test_it_exists
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_instance_of Recipe, recipe1
+  end
 end
