9adf275c6baa67dcd4304c190add6b8462d2dc4d
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 09:40:13 -0600

    Update test_it_can_restock_ingredients

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -29,10 +29,12 @@ class PantryTest < Minitest::Test
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
 
+    assert_equal ({ ingredient1 => 15 }), pantry.stock
     assert_equal 15, pantry.stock_check(ingredient1)
 
     pantry.restock(ingredient2, 7)
 
+    assert_equal ({ ingredient1 => 15, ingredient2 => 7 }), pantry.stock
     assert_equal 7, pantry.stock_check(ingredient2)
   end
 end
