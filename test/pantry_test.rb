f8324e4e27ac4730705e89619872c17bc31c36e2
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 09:26:34 -0600

    Add test_it_has_attributes

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,4 +8,14 @@ class PantryTest < Minitest::Test
 
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_has_attributes
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
 end
+
+# pantry = Pantry.new
+# ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+# ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
