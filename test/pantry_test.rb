41a1197b712cdead0b1bcb65af0c224e908e8db2
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 10:40:40 -0600

    Add test_has_enough_ingredients

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/pantry'
+require './lib/cook_book'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
   def test_it_exists
@@ -36,5 +38,30 @@ class PantryTest < Minitest::Test
 
     assert_equal ({ ingredient1 => 15, ingredient2 => 7 }), pantry.stock
     assert_equal 7, pantry.stock_check(ingredient2)
+  end
+
+  def test_has_enough_ingredients
+    pantry = Pantry.new
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    cookbook.add_recipe(recipe1)
+
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+
+    pantry.restock(ingredient2, 7)
+
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+
+    pantry.restock(ingredient2, 1)
+
+    assert pantry.enough_ingredients_for?(recipe1)
   end
 end
