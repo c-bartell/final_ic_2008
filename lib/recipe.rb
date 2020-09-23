406bd1d6adceac799581a7a611bfa5ae6e4b405a
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 09:54:43 -0600

    Add add_ingredient

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -5,6 +5,10 @@ class Recipe
 
   def initialize(name)
     @name = name
-    @ingredients_required = {}
+    @ingredients_required = Hash.new(0)
+  end
+
+  def add_ingredient(ingredient, amount)
+    @ingredients_required[ingredient] += amount
   end
 end
