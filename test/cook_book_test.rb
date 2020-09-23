ce53df96281a0324e5dd4f4e45a83a0e5fd23d7e
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 10:00:31 -0600

    Add test_it_exists

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -3,4 +3,9 @@ require 'minitest/pride'
 require './lib/cookbook'
 
 class CookBookTest < Minitest::Test
+  def test_it_exists
+    cookbook = CookBook.new
+
+    assert_instance_of CookBook, cookbook
+  end
 end
