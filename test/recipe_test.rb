eecde0da7ea0541a494a058521a9951e06058530
Curtis Bartell <cbartell980@gmail.com>
2020-09-23 10:21:44 -0600

    Update test_it_can_calculate_total_calories

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -41,12 +41,19 @@ class RecipeTest < Minitest::Test
 
   def test_it_can_calculate_total_calories
     recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     recipe1.add_ingredient(ingredient1, 2)
-    recipe1.add_ingredient(ingredient1, 4)
     recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
 
-    assert_equal 840, recipe1.total_calories
+
+    assert_equal 440, recipe1.total_calories
+    assert_equal 675, recipe2.total_calories
   end
 end
